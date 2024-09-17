011acdd9ca093ebe29e479e657cb7bf08d093534
NR <95234863+sraww@users.noreply.github.com>
2024-09-17 18:10:03 +0300

    Update ShowMemoryVeight.cs

--- NeiroNet1/ShowMemoryVeight.cs
+++ NeiroNet1/ShowMemoryVeight.cs
@@ -21,7 +21,7 @@ namespace NeiroNet1
         private void ShowMemoryVeight_Load(object sender, EventArgs e)
         {
             dataGridView1.ColumnCount = neiron.veight.GetLength(0);
-            dataGridView1.RowCount    = neiron.veight.GetLength(1);
+            dataGridView1.RowCount  = neiron.veight.GetLength(1);
             dataGridView1.DefaultCellStyle.ForeColor = Color.Green;
             // Для новой генерации обьекта. ТЕСТ)
             for (int n = 0; n < neiron.veight.GetLength(0); n++)

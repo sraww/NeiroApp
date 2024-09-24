45b682224b0fa2ae8ea7d1bf95653ee70b7e8ae8
NR <95234863+sraww@users.noreply.github.com>
2024-09-24 18:01:06 +0300

    Update Resources.Designer.cs

--- NeiroNet1/Properties/Resources.Designer.cs
+++ NeiroNet1/Properties/Resources.Designer.cs
@@ -23,7 +23,6 @@ namespace NeiroNet1.Properties {
     internal class Resources {
         
         private static global::System.Resources.ResourceManager resourceMan;
-        
         private static global::System.Globalization.CultureInfo resourceCulture;
         
         [global::System.Diagnostics.CodeAnalysis.SuppressMessageAttribute("Microsoft.Performance", "CA1811:AvoidUncalledPrivateCode")]

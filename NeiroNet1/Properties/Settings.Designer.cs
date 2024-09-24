603a29018301262a03b7dd68333fa5d6526923d1
NR <95234863+sraww@users.noreply.github.com>
2024-09-24 18:00:04 +0300

    Update Settings.Designer.cs

--- NeiroNet1/Properties/Settings.Designer.cs
+++ NeiroNet1/Properties/Settings.Designer.cs
@@ -9,7 +9,6 @@
 //------------------------------------------------------------------------------
 namespace NeiroNet1.Properties {
     
-    
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
     [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "17.6.0.0")]
     internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase {

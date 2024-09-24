d2ed6e480b58c5329e1c5ddb89fc3c512ea38099
NR <95234863+sraww@users.noreply.github.com>
2024-09-24 18:00:23 +0300

    Update Settings.Designer.cs

--- NeiroNet1/Properties/Settings.Designer.cs
+++ NeiroNet1/Properties/Settings.Designer.cs
@@ -12,8 +12,7 @@ namespace NeiroNet1.Properties {
     [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "17.6.0.0")]
     internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase {
         
-        private static Settings defaultInstance = ((Settings)(global::System.Configuration.ApplicationSettingsBase.Synchronized(new Settings())));
-        
+        private static Settings defaultInstance = ((Settings)(global::System.Configuration.ApplicationSettingsBase.Synchronized(new Settings())));   
         public static Settings Default {
             get {
                 return defaultInstance;

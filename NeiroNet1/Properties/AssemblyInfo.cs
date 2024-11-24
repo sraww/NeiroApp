b9b357214e24e544c644ccd98de67fb39f314085
NR <95234863+sraww@users.noreply.github.com>
2024-11-24 10:04:07 +0300

    Update AssemblyInfo.cs

--- NeiroNet1/Properties/AssemblyInfo.cs
+++ NeiroNet1/Properties/AssemblyInfo.cs
@@ -1,6 +1,7 @@
 ﻿using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
+
 // Управление общими сведениями о сборке осуществляется с помощью 
 // набора атрибутов. Измените значения этих атрибутов, чтобы изменить сведения,
 // связанные со сборкой.

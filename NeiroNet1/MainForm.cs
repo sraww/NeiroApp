86b2c51f69cd41d8b2a19aa493458964f962c34a
NR <95234863+sraww@users.noreply.github.com>
2025-06-13 10:08:33 +0300

    Update MainForm.cs

--- NeiroNet1/MainForm.cs
+++ NeiroNet1/MainForm.cs
@@ -106,7 +106,6 @@ namespace NeiroNet1
             NeiroGraphUtils.ClearImage(pictureBox3);
         }
         
-
         private void learnToolStripMenuItem_Click(object sender, EventArgs e)
         {
             Learn();

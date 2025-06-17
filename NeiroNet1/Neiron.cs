59acd74c5b4ab9b793607b64a78595b567ee3108
NR <95234863+sraww@users.noreply.github.com>
2025-06-17 20:13:10 +0300

    Update Neiron.cs

--- NeiroNet1/Neiron.cs
+++ NeiroNet1/Neiron.cs
@@ -5,8 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace NeiroNet1
-   
-{
+   {
    // это класс нейрона, каждый нейрон хранит в себе массив определённого образа может обучаться и сравнивать значение с имеющимся в памяти
     public class Neiron
     {
